f13dc51f1036e3fe4235f9b70a59c22d856ec421
Prajwol Basnet <prajal.basnet0@gmail.com>
2022-12-06 13:33:13 +0545

    react-bootstrap-started

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -5,6 +5,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css" //bootstrap css
 // import "bootstrap"//full bootstrap js
+import { Nav, Container, Navbar, Carousel } from "react-bootstrap";
 
 class HomePage extends React.Component {
 
@@ -28,17 +29,59 @@ class HomePage extends React.Component {
     render = () => {
         // view presentation
         return (
-            <p>
-                Your name is: {this.state.name}.
-                <br />
-                Your address is: {this.state.address}.
-                <br />
-                Your Props value is: {this.props.name}.
-                <br />
-                Your Date From Props is: {this.props.data.name + '' + this.props.data.email}
-                <br />
-                <button onClick={this.testFunction}>Update Me </button>
-            </p>
+            // react fragment
+            <>
+                <Navbar bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+                <Carousel fade>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=First slide&bg=373940"
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=Second slide&bg=282c34"
+                            alt="Second slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=Third slide&bg=20232a"
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                            </p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+
+            </>
         )
     }
 }
