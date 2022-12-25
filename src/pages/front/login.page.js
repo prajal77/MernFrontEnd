c6478e83defec7842cece51c96568df39b54fa6b
Prajwol Basnet <prajal.basnet0@gmail.com>
2022-12-25 13:16:31 +0545

    fromit_implemented

--- src/pages/front/login.page.js
+++ src/pages/front/login.page.js
@@ -1,8 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 const LoginPage = () => {
     // let [email, setEmail] = useState(null);
+
     // let [password, setPassword] = useState(null);
     // let [rememberMe, setRememberMe] = useState(false);
 
@@ -11,11 +14,11 @@ const LoginPage = () => {
         password: "",
         rememberMe: false
     }
-    let [data, setData] = useState(defaultData);
-    let [err, setErr] = useState({
-        email: '',
-        password: ''
-    });
+    // let [data, setData] = useState(defaultData);
+    // let [err, setErr] = useState({
+    //     email: '',
+    //     password: ''
+    // });
 
     // First way of useing useEffect hook
     // useEffect(() => {
@@ -31,42 +34,53 @@ const LoginPage = () => {
     // useEffect(() => {
     //     // this hook only execues when the data state is updated
     // }, [data]);
-    const handleChange = (e) => {
-        let { name, value } = e.target;
-        setData({
-            ...data,
-            [name]: value //[] gives us the key value
-        })
-        validateData(name, value);
-    }
+    // const handleChange = (e) => {
+    //     let { name, value } = e.target;
+    //     setData({
+    //         ...data,
+    //         [name]: value //[] gives us the key value
+    //     })
+    //     validateData(name, value);
+    // }
 
-    const validateData = (field, value) => {
-        let msg = "";
-        switch (field) {
-            case 'email':
-                msg = !value ? "Email is required" :
-                    (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
-                        .test(value) ? "" : "invalid email Format")
-                break;
-            case 'password':
-                msg = !value ? "Password is required" : (value.length < 9 ? "Password must be 8 character long" : "");
-                break;
-            default:
-                break;
+    // const validateData = (field, value) => {
+    //     let msg = "";
+    //     switch (field) {
+    //         case 'email':
+    //             msg = !value ? "Email is required" :
+    //                 (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+    //                     .test(value) ? "" : "invalid email Format")
+    //             break;
+    //         case 'password':
+    //             msg = !value ? "Password is required" : (value.length < 9 ? "Password must be 8 character long" : "");
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    //     setErr({
+    //         ...err,
+    //         [field]: msg
+    //     })
+    // }
+
+    // const handleSubmit = (e) => {
+    //     e.prevenDefault();
+    //     // TODO API call to integration
+    // }
+
+    // console.log(data);
+
+    let loginValidationSchema = Yup.object().shape({
+        email: Yup.string().email("Invalid Email format").required("Email is required"),
+        password: Yup.string().required("Password is required")
+    });
+    let formik = useFormik({
+        initialValues: defaultData,
+        validationSchema: loginValidationSchema,
+        onSubmit: (values) => {
+            console.log("value:", values)
         }
-        setErr({
-            ...err,
-            [field]: msg
-        })
-    }
-
-    const handleSubmit = (e) => {
-        e.prevenDefault();
-        // TODO API call to integration
-    }
-
-    console.log(data);
-
+    })
     return (
         <>
             <Container>
@@ -74,7 +88,7 @@ const LoginPage = () => {
                     <Col sm={{ offset: 3, span: 6 }}>
                         <h4>Login Page</h4>
                         <hr />
-                        <Form onSubmit={handleSubmit}>
+                        <Form onSubmit={formik.handleSubmit}>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <Form.Label>Email address</Form.Label>
                                 <Form.Control
@@ -83,26 +97,38 @@ const LoginPage = () => {
                                     name="email"
                                     size="sm"
                                     required
-                                    onChange={(e) => {
-                                        let value = e.target.value;
-                                        setData({
-                                            ...data,
-                                            email: value
-                                        })
-                                        validateData("email", value)
-                                    }}
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange}
                                 />
-                                <em className="text-danger">{err?.email}</em>
+                                {
+                                    formik.errors.email && <em className="text-danger">{formik.errors.email}</em>
+
+                                }
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                 <Form.Label>Password</Form.Label>
                                 <Form.Control type="password" placeholder="Password" name="password" size="sm" required
-                                    onChange={handleChange} />
-                                <em className="text-danger">{err?.password}</em>
+                                    onChange={formik.handleChange} />
+                                {
+                                    formik.errors.password && <em className="text-danger">{formik.errors.password}</em>
+
+                                }
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                <Form.Check type="checkbox" label="Remember Me" name="remember me" />
+                                <Form.Check type="checkbox" label="Remember Me" name="remember me" onChange={(e) => {
+                                    if (e.target.checked) {
+                                        formik.setValues({
+                                            ...formik.values,
+                                            rememberMe: true
+                                        })
+                                    } else {
+                                        formik.setValues({
+                                            ...formik.values,
+                                            rememberMe: false
+                                        })
+                                    }
+                                }} />
                             </Form.Group>
                             <div className="d-grid">
                                 <Button variant="success" type="submit" size="sm">
--- src/pages/layouts/admin-layout.page.js
+++ src/pages/layouts/admin-layout.page.js
@@ -2,6 +2,7 @@
 import "../../assets/admin/css/admin.css";
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import "bootstrap";
+import { Outlet } from "react-router-dom";
 
 const AdminLayout = () => {
     const sidebarToggle = (e) => {
@@ -108,27 +109,8 @@ const AdminLayout = () => {
             </div>
             <div id="layoutSidenav_content">
                 <main>
-                    <div className="container-fluid px-4">
-                        <h1 className="mt-4">Static Navigation</h1>
-                        <ol className="breadcrumb mb-4">
-                            <li className="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                            <li className="breadcrumb-item active">Static Navigation</li>
-                        </ol>
-                        <div className="card mb-4">
-                            <div className="card-body">
-                                <p className="mb-0">
-                                    This page is an example of using static navigation. By removing the
-                                    <code>.sb-nav-fixed</code>
-                                    className from the
-                                    <code>body</code>
-                                    , the top navigation and side navigation will become static on scroll. Scroll down this page to see an example.
-                                </p>
-                            </div>
-                        </div>
-                        {/* <div style="height: 100vh"></div> */}
-                        <div style={{ height: '100vh' }}></div>
-                        <div className="card mb-4"><div className="card-body">When scrolling, the navigation stays at the top of the page. This is the end of the static navigation demo.</div></div>
-                    </div>
+                    <Outlet />
+
                 </main>
                 <footer className="py-4 bg-light mt-auto">
                     <div className="container-fluid px-4">
